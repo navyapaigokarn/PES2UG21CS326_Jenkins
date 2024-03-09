44493987e12af167cc52ea6382f6d4d87a3c4edf
navyapaigokarn <137099339+navyapaigokarn@users.noreply.github.com>
2024-03-09 14:56:46 +0530

    Create PES2UG21CS326-1.cpp

---
+++ PES2UG21CS326-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "This is PES2UG21CS326" << std::endl;
+
+    for (int i = 1; i <= 5; ++i) {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
